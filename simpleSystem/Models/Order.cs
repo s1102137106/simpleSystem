6abe9e519f1297d4e147a610b39b60c881dd8a39
s1102137106 <s56565566123@gmail.com>
2016-03-21 22:51:47 +0800

    add model

---
+++ simpleSystem/Models/Order.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace eSale.Models
+{
+    public class Order
+    {
+        /// <summary>
+        /// 訂單編號
+        /// </summary>
+        
+        [DisplayName("訂單編號")]
+        [Required()]
+        public int OrderDd {get;set;}
+
+        /// <summary>
+        /// 客戶代號
+        /// </summary>
+        [MaxLength(3)]
+        [DisplayName("客戶代號")]
+        public string CustId {get;set;}
+
+        /// <summary>
+        /// 客戶名稱
+        /// </summary>
+        public string CustName { get; set; }
+
+        /// <summary>
+        /// 業務(員工)代號
+        /// </summary>
+        public int EmpId {get;set;}
+
+        /// <summary>
+        /// 業務(員工姓名)
+        /// </summary>
+        public string EmpName { get; set; }
+
+        /// <summary>
+        /// 訂單日期
+        /// </summary>
+        public DateTime ? Orderdate {get;set;}
+
+        /// <summary>
+        /// 需要日期
+        /// </summary>
+        public DateTime? RequireDdate { get; set; }
+
+        /// <summary>
+        /// 出貨日期
+        /// </summary>
+        public DateTime?  ShippedDate { get; set; }
+
+        /// <summary>
+        /// 出貨公司代號
+        /// </summary>
+        public int ShipperId {get;set;}
+
+        /// <summary>
+        /// 出貨公司名稱
+        /// </summary>
+        public int ShipperName { get; set; }
+
+        /// <summary>
+        /// 運費
+        /// </summary>
+        /// 
+        [Range(0,double.MaxValue)]
+        [DisplayName("運費")]
+        public double Freight {get;set;}
+
+        /// <summary>
+        /// 出貨說明
+        /// </summary>
+        public string ShipName {get;set;}
+
+        /// <summary>
+        /// 出貨地址
+        /// </summary>
+        public string ShipAddress {get;set;}
+
+        /// <summary>
+        /// 出貨程式
+        /// </summary>
+        public string ShipCity {get;set;}
+
+        /// <summary>
+        /// 出貨地區
+        /// </summary>
+        public string ShipRegion {get;set;}
+
+        /// <summary>
+        /// 郵遞區號
+        /// </summary>
+        public string ShipPostalCode {get;set;}
+
+        /// <summary>
+        /// 出貨國家
+        /// </summary>
+        public string ShipCountry { get; set; }
+    }
+}
---
+++ simpleSystem/Models/OrderController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eSale.Controllers
+{
+    public class OrderController : Controller
+    {
+
+        /// <summary>
+        /// 訂單管理首頁
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 訂單管理明細
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Detial()
+        {
+
+            return View();
+        }
+
+        [HttpGet()]
+        public ActionResult InsertOrder()
+        {
+            return View();
+        }
+
+        public ActionResult GetSysData()
+        {
+            return PartialView("_SysDatePartial");
+        }
+
+        [HttpPost()]
+        public ActionResult InsertOrder(Models.Order order)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.Hello = "ViewBag歡迎光臨";
+                TempData["Hello"] = "TempData歡迎光臨";
+                return RedirectToAction("Index");
+                
+            }
+            return View(order);
+            //return View();
+        }
+
+        public JsonResult TestJsonResult()
+        {
+            Models.OrderService service = new Models.OrderService();
+            return this.Json(service.GetOrderById("123"), JsonRequestBehavior.AllowGet);
+        }
+    }
+}
