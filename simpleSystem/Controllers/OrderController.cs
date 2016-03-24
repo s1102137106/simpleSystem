0963b7f465ee5d16077292b833d5e4b5d6f9e7c1
s1102137106 <s56565566123@gmail.com>
2016-03-24 10:28:03 +0800

    order前端完成 剩下後台資料

--- simpleSystem/Controllers/OrderController.cs
+++ simpleSystem/Controllers/OrderController.cs
@@ -13,8 +13,47 @@ namespace eSale.Controllers
         /// 訂單管理首頁
         /// </summary>
         /// <returns></returns>
+
+        [HttpGet()]
         public ActionResult Index()
         {
+            Models.OrderService orderService = new Models.OrderService();
+            List<Models.Order> orderList = new List<Models.Order>();
+            Models.Order nullCondition = new Models.Order();
+            //查詢條件設定成空的
+            orderList = orderService.GetOrderByCondtioin(nullCondition);
+            ViewBag.Data = orderList;
+
+            List<SelectListItem> EmpId = new List<SelectListItem>();//業務(員工)代號
+            List<SelectListItem> ShipperId = new List<SelectListItem>();//出貨公司代號
+
+            foreach (Models.Order order in orderList)
+            {
+                EmpId.Add(new SelectListItem
+                {
+                    Text = order.EmpId.ToString(),
+                    Value = order.EmpId.ToString()
+                });
+
+                ShipperId.Add(new SelectListItem
+                {
+                    Text = order.ShipperId.ToString(),
+                    Value = order.ShipperId.ToString()
+                });
+            }
+            ViewBag.EmpIds = EmpId;
+            ViewBag.ShipperIds = ShipperId;
+
+            return View();
+        }
+
+        [HttpPost()]
+        public ActionResult Index(Models.Order condition)
+        {
+            Models.OrderService orderService = new Models.OrderService();
+            List<Models.Order> orderList = new List<Models.Order>();
+            orderList = orderService.GetOrderByCondtioin(condition);
+            ViewBag.Data = orderList;
             return View();
         }
 
@@ -47,7 +86,7 @@ namespace eSale.Controllers
                 ViewBag.Hello = "ViewBag歡迎光臨";
                 TempData["Hello"] = "TempData歡迎光臨";
                 return RedirectToAction("Index");
-                
+
             }
             return View(order);
             //return View();
--- simpleSystem/Models/OrderService.cs
+++ simpleSystem/Models/OrderService.cs
@@ -29,12 +29,14 @@ namespace Models
         /// 依照條件取得訂單資料
         /// </summary>
         /// <returns></returns>
-        public List<Models.Order> GetOrderByCondtioin()
+        public List<Models.Order> GetOrderByCondtioin(Models.Order order)
         {
             //todo
             List<Models.Order> result = new List<Order>();
-            result.Add(new Order() { CustId = "001", CustName = "叡揚資訊", EmpId = 1, EmpName = "王小明", Orderdate = DateTime.Parse("2015/11/08") });
-            result.Add(new Order() { CustId = "002", CustName = "網軟資訊", EmpId = 2, EmpName = "李小華", Orderdate = DateTime.Parse("2015/11/01") });
+            Console.WriteLine(order);
+            //依照給的order資訊下條件
+            result.Add(new Order() { CustId = "001", CustName = "叡揚資訊", EmpId = 1, EmpName = "王小明", Orderdate = DateTime.Parse("2015/11/08"), ShippedDate=DateTime.Parse("2015/11/09") });
+            result.Add(new Order() { CustId = "002", CustName = "網軟資訊", EmpId = 2, EmpName = "李小華", Orderdate = DateTime.Parse("2015/11/01"), ShippedDate = DateTime.Parse("2015/11/09") });
             return result;
         }
         /// <summary>
